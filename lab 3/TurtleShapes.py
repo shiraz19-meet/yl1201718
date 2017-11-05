3186bdf35ac90537f1394a166ffde3b7f32c860a
adan19-meet <adan19@meet.mit.edu>
2017-11-05 17:59:24 +0200

    hey im lazy shiraz that dont know how to push things into github

--- lab 3/TurtleShapes.py
+++ lab 3/TurtleShapes.py
@@ -1,9 +1,24 @@
-import turtle
-turtle.speed(100)
-turtle.pensize(10)
+ import turtle
+ turtle.speed(100)
+ turtle.pensize(10)
 
-for i in range(5):
-    turtle.forward(100)
-    turtle.right(140)
-turtle.mainloop
+# for i in range(5):
+#     turtle.forward(100)
+#     turtle.right(140)
+# turtle.mainloop
         
+##turtle.forward(50)
+##turtle.right(90)
+##turtle.forward(50)
+##turtle.right(60)
+##turtle.forward(50)
+##turtle.right(60)
+##turtle.forward(50)
+##turtle.right(60)
+##turtle.forward(50)
+##turtle.right(90)
+##turtle.forward(50)
+
+
+turtle.register_shape('weird',((0,0), (0,50), (50,50)))
+turtle.mainloop
